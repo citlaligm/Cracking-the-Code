355b0d7180258b55ccb12d80b4cd273ec0939c5c
Citlali <gmtlali88@gmail.com>
2018-03-12 14:23:08 -0700

    Add tests

--- Chapter-1/python/ex_1_3.py
+++ Chapter-1/python/ex_1_3.py
@@ -1,9 +1,10 @@
 def URLify(str_in,n):
-	ulr_part = str_in.split()
-	return "%20".join(ulr_part)
+    ulr_part = str_in.split()
+    return "%20".join(ulr_part)
 
-str_in = "Mr John Smith"
 
-print(URLify(str_in,13))
-
+def test_URLify():
+    str_in = "Mr John Smith"
+    str_out = "Mr%20John%20Smith"
+    assert URLify(str_in,13) == str_out
 
