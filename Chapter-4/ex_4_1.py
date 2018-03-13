396b8eaffe470caa86cbf0d1fb6394c6da23012e
Citlali <gmtlali88@gmail.com>
2018-03-13 15:03:29 -0700

    Adds code for dfs and bfs solution

--- Chapter-4/ex_4_1.py
+++ Chapter-4/ex_4_1.py
@@ -1,16 +1,38 @@
 from Graph import Graph, Node
+from collections import deque
 
-
-def Route_nodes(graph):
+def Route_nodes(graph, init_node=None, end_node=None):
     if len(graph.nodes) == 0:
         return False
     
+    if (init_node.name == end_node.name):
+        return True
+    
     #Implement DFS recursively
-    while len(graph.nodes) > 0:
+    for node in init_node.adjacents:       
+        if not node.visited:  
+            node.visited = True  
+            return Route_nodes(graph, node, end_node)
+   
+    return False
+    
+    
+def Route_bfs_deque(graph, init_node=None, end_node=None, queue=deque()):
+    
+    init_node.visited = True
+    queue.append(init_node)
+    
+    while queue:
+        node = queue.pop()
+        if (node.name == end_node.name):
+            return True
+        for node in node.adjacents:
+            if node.visited == False:
+                queue.append(node)
+                node.visited = True
         
     
-    
-    
+    return False
     
     
 def test_empty():
@@ -19,12 +41,68 @@ def test_empty():
 
     assert Route_nodes(graph) == False
     
-def test_thereis_a_route():
+def test_base_case():
     node6 = Node(6, '6')
     node5 = Node(5, '5')
     node4 = Node(4, '4')
 
+    node6.adjacents = [node5]
+    node5.adjacents = [node4]
+    node4.adjacents = [node6]
+    
     graph = Graph([node6, node5, node4])
     
 
-    assert Route_nodes(graph) == True
+    assert Route_nodes(graph, node6, node6) == True
+
+def test_thereis_a_path():
+    node6 = Node(6, '6')
+    node5 = Node(5, '5')
+    node4 = Node(4, '4')
+
+    node6.adjacents = [node5]
+    node5.adjacents = [node4]
+    node4.adjacents = [node6]
+    
+    graph = Graph([node6, node5, node4])
+    
+
+    assert Route_nodes(graph, node6, node4) == True
+
+def test_thereis_nota_path_dfs():
+    node6 = Node(6, '6')
+    node5 = Node(5, '5')
+    node4 = Node(4, '4')
+    node3 = Node(3, '3')
+    node2 = Node(2, '2')
+
+    node6.adjacents = [node5]
+    node5.adjacents = [node3]
+    node4.adjacents = [node2]
+    
+    graph = Graph([node6, node5, node4, node3])
+    
+
+    assert Route_nodes(graph, node6, node4) == False
+    #assert Route_bfs_deque(graph, node6, node4) == False
+    
+    
+def test_thereis_nota_path_bfs():
+    node6 = Node(6, '6')
+    node5 = Node(5, '5')
+    node4 = Node(4, '4')
+    node3 = Node(3, '3')
+    node2 = Node(2, '2')
+
+    node6.adjacents = [node5]
+    node5.adjacents = [node3]
+    node4.adjacents = [node2]
+    
+    graph = Graph([node6, node5, node4, node3])
+    
+
+    assert Route_bfs_deque(graph, node6, node4) == False
+    
+    
+
+
